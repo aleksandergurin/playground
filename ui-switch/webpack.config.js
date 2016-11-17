93f0bf759c9c773449aec4cbc694a6aa0b1b1c9a
Aleksander Gurin <alek.gurin@gmail.com>
2016-11-17 23:05:56 +0200

    Added ui-switch component

---
+++ ui-switch/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    entry: "./src/index.js",
+
+    output: {
+        path: "build/",
+        filename: "bundle.js",
+        publicPath: "build/"
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: '/node-modules',
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'stage-2', 'react']
+                },
+            },
+            {
+                test: /\.styl$/,
+                exclude: '/node_modules',
+                loader: 'style!css!stylus',
+            }
+        ]
+    }
+};
