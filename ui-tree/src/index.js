a22beaeef44ccff1bedf634fe45aca15b1fed67e
a.gurin <a.gurin@smartweb.com.ua>
2016-11-17 12:56:20 +0200

    Cleanup and minor improvements in ui-tree

--- ui-tree/src/index.js
+++ ui-tree/src/index.js
@@ -8,116 +8,122 @@ import {
     toggleSelectDeselect,
     filterTreeNodes
 } from './actions';
+
 import {treeReducer} from './reducer';
 
-const storage = {
-    originalTreeCache: null,
 
-    tree: {
-        id: '1',
-        data: 'First level',
-        collapsed: false,
-        children: [
-            {
-                id: '1.1',
-                data: 'Second level A',
-                collapsed: false,
-                children: [
-                    {
-                        id: '1.1.2',
-                        data: 'Third level A',
-                        collapsed: false,
-                        children: [
-                            {
-                                id: '1.1.1.1',
-                                data: 'Forth level A',
-                                collapsed: false,
-                                children: [
-                                    {
-                                        id: '1.1.1.1.1',
-                                        data: 'Fifth level A',
-                                        collapsed: false,
-                                    },
-                                    {
-                                        id: '1.1.1.1.2',
-                                        data: 'Fifth level B',
-                                        collapsed: false,
-                                    },
-                                    {
-                                        id: '1.1.1.1.3',
-                                        data: 'Fifth level C',
-                                        collapsed: false,
-                                    },
-                                    {
-                                        id: '1.1.1.1.4',
-                                        data: 'Fifth level D',
-                                        collapsed: false,
-                                    },
-                                ]
-                            },
-                            {
-                                id: '1.1.1.2',
-                                data: 'Forth level B',
-                                collapsed: false,
-                            },
-                            {
-                                id: '1.1.1.3',
-                                data: 'Forth level C',
-                                collapsed: false,
-                            },
-                        ],
-                    },
-                    {
-                        id: '1.1.3',
-                        data: 'Third level B',
-                        collapsed: false,
-                    },
-                    {
-                        id: '1.1.4',
-                        data: 'Third level C',
-                        collapsed: false,
-                    },
-                    {
-                        id: '1.1.5',
-                        data: 'Third level D',
-                        collapsed: false,
-                    },
-                ]
-            },
-            {
-                id: '1.2',
-                data: 'Second level B',
-                collapsed: false,
-            },
-            {
-                id: '1.3',
-                data: 'Second level C',
-                collapsed: false,
-            },
-        ]
-    }
+const treeFromServe = {
+    id: '1',
+    data: null,
+    collapsed: false,
+    children: [
+        {
+            id: '1.1',
+            data: 'Second level A',
+            collapsed: false,
+            children: [
+                {
+                    id: '1.1.2',
+                    data: 'Third level A',
+                    collapsed: false,
+                    children: [
+                        {
+                            id: '1.1.1.1',
+                            data: 'Forth level A',
+                            collapsed: false,
+                            children: [
+                                {
+                                    id: '1.1.1.1.1',
+                                    data: 'Fifth level A',
+                                    collapsed: false,
+                                },
+                                {
+                                    id: '1.1.1.1.2',
+                                    data: 'Fifth level B',
+                                    collapsed: false,
+                                },
+                                {
+                                    id: '1.1.1.1.3',
+                                    data: 'Fifth level C',
+                                    collapsed: false,
+                                },
+                                {
+                                    id: '1.1.1.1.4',
+                                    data: 'Fifth level D',
+                                    collapsed: false,
+                                },
+                            ]
+                        },
+                        {
+                            id: '1.1.1.2',
+                            data: 'Forth level B',
+                            collapsed: false,
+                        },
+                        {
+                            id: '1.1.1.3',
+                            data: 'Forth level C',
+                            collapsed: false,
+                        },
+                    ],
+                },
+                {
+                    id: '1.1.3',
+                    data: 'Third level B',
+                    collapsed: false,
+                },
+                {
+                    id: '1.1.4',
+                    data: 'Third level C',
+                    collapsed: false,
+                },
+                {
+                    id: '1.1.5',
+                    data: 'Third level D',
+                    collapsed: false,
+                },
+            ]
+        },
+        {
+            id: '1.2',
+            data: 'Second level B',
+            collapsed: false,
+        },
+        {
+            id: '1.3',
+            data: 'Second level C',
+            collapsed: false,
+        },
+    ]
 };
 
-const TOGGLE_COLLAPSE_EXPAND = 'TOGGLE_COLLAPSE_EXPAND';
+function initUiTree(initialTree) {
 
+    const store = createStore(
+        treeReducer,
+        {
+            originalTreeCache: null,
+            tree: initialTree,
+            selectedItems: [],
+        }
+    );
 
-const store = createStore(treeReducer, storage);
+    const render = () => ReactDOM.render(
+        <div>
+            <input
+                type="text"
+                onChange={(e) => store.dispatch(filterTreeNodes(e.target.value))}
+            />
+            <Tree
+                {...store.getState().tree}
+                onNodeClick={(nodeId) => store.dispatch(toggleCollapseExpand(nodeId))}
+                onNodeSelect={(nodeId) => store.dispatch(toggleSelectDeselect(nodeId))}
+            />
+        </div>,
+        document.getElementById('example')
+    );
+    render();
 
+    store.subscribe(render);
+}
 
-const render = () => ReactDOM.render(
-    <div>
-        <input
-            type="text"
-            onChange={(e) => store.dispatch(filterTreeNodes(e.target.value))}
-        />
-        <Tree
-            {...store.getState().tree}
-            onNodeClick={(nodeId) => store.dispatch(toggleCollapseExpand(nodeId))}
-            onNodeSelect={(nodeId) => store.dispatch(toggleSelectDeselect(nodeId))}
-        />
-    </div>,
-    document.getElementById('example')
-);
-render();
-
-store.subscribe(render);
+initUiTree(treeFromServe);
--- ui-tree/src/reducer.js
+++ ui-tree/src/reducer.js
@@ -3,111 +3,136 @@ import {
     TOGGLE_SELECT_DESELECT,
     FILTER_TREE_NODES,
 } from './actions';
+
 import * as NODE_ENUM from './enums';
 
 
-function toggleCollapseNode(node, nodeId) {
-    const {children = []} = node;
+function toggleCollapseTreeNode(state, action) {
+    function collapseRecursive(node, nodeId) {
+        const {children = []} = node;
 
-    return {
-        ...node,
-        collapsed: (nodeId === node.id) ? !node.collapsed : node.collapsed,
-        children: children.map(n => toggleCollapseNode(n, nodeId))
-    };
+        return {
+            ...node,
+            collapsed: (nodeId === node.id) ? !node.collapsed : node.collapsed,
+            children: children.map(n => collapseRecursive(n, nodeId))
+        };
+    }
+
+    return {...state, tree: collapseRecursive(state.tree, action.nodeId)};
 }
 
 
-function toggleSelectNode(node, nodeId) {
+function toggleSelectionOfTreeNode(state, action) {
+    function toggleSelectionInner(node, nodeId) {
+        // Utilitarian functions
 
-    function setAll(node, selected) {
-        const {children = []} = node;
-        return {
-            ...node,
-            selected,
-            children: children.map(n => setAll(n, selected)),
-        }
-    }
+        const hasSelectedParent = ({selected = NODE_ENUM.NODE_DESELECTED} = {}) => (
+            selected === NODE_ENUM.NODE_HAS_SELECTED_PARENT
+        );
 
-    function processOneNode(node, id) {
-        let selected = node.selected || NODE_ENUM.NODE_DESELECTED;
-        if (node.id === id) {
-            switch (selected) {
-                case NODE_ENUM.NODE_HAS_SELECTED_CHILDREN: // fallthrough
-                case NODE_ENUM.NODE_DESELECTED:
-                    return {
-                        ...setAll(node, NODE_ENUM.NODE_HAS_SELECTED_PARENT),
-                        selected: NODE_ENUM.NODE_SELECTED,
-                    };
-                case NODE_ENUM.NODE_HAS_SELECTED_PARENT: // fallthrough
-                case NODE_ENUM.NODE_SELECTED:
-                    return {
-                        ...setAll(node, NODE_ENUM.NODE_DESELECTED),
-                    };
-                default:
-                    return {...node};
+        const hasSelectedChildren = ({selected = NODE_ENUM.NODE_DESELECTED} = {}) => (
+            selected === NODE_ENUM.NODE_HAS_SELECTED_CHILDREN
+        );
+
+        const isDeselected = ({selected = NODE_ENUM.NODE_DESELECTED} = {}) => (
+            selected === NODE_ENUM.NODE_DESELECTED
+        );
+
+        const isSelected = ({selected = NODE_ENUM.NODE_DESELECTED} = {}) => (
+            selected === NODE_ENUM.NODE_SELECTED
+        );
+
+        const setAll = (node, selected) => {
+            const {children = []} = node;
+            return {
+                ...node, selected,
+                children: children.map(n => setAll(n, selected)),
             }
-        }
+        };
 
-        const {children = []} = node;
-        const newChildren = children.map(n => processOneNode(n, nodeId));
+        // This is the function that does main work.
+        function toggleSelectionRecursive(node, id) {
+            const selected = node.selected || NODE_ENUM.NODE_DESELECTED;
 
-        if (newChildren.length > 0) {
-            if (newChildren.every(n => n.selected === NODE_ENUM.NODE_SELECTED)) {
-                return {
-                    ...node,
-                    selected: NODE_ENUM.NODE_SELECTED,
-                    children: newChildren.map(n => ({...n, selected: NODE_ENUM.NODE_HAS_SELECTED_PARENT})),
-                }
-            } else if (newChildren.every(
-                    n => n.selected === NODE_ENUM.NODE_DESELECTED || n.selected === undefined
-                )
-            ) {
-                return {
-                    ...node,
-                    selected: NODE_ENUM.NODE_DESELECTED,
-                    children: newChildren,
-                }
-            } else if (
-                newChildren.some(
-                    n => (
-                        n.selected === NODE_ENUM.NODE_SELECTED ||
-                        n.selected === NODE_ENUM.NODE_HAS_SELECTED_CHILDREN
-                    )
-                )
-            ) {
-                return {
-                    ...node,
-                    selected: NODE_ENUM.NODE_HAS_SELECTED_CHILDREN,
-                    children: newChildren.map(n => {
-                        if (n.selected === NODE_ENUM.NODE_HAS_SELECTED_PARENT) {
-                            return {...n, selected: NODE_ENUM.NODE_SELECTED};
-                        } else {
-                            return {...n}
-                        }
-                    }),
-                }
-            } else if (
-                newChildren.some(n => n.selected === NODE_ENUM.NODE_HAS_SELECTED_PARENT) &&
-                newChildren.some(n => n.selected === NODE_ENUM.NODE_DESELECTED || n.selected === undefined)
-            ) {
-                return {
-                    ...node,
-                    selected: NODE_ENUM.NODE_HAS_SELECTED_CHILDREN,
-                    children: newChildren.map(n => {
-                        if (n.selected === NODE_ENUM.NODE_HAS_SELECTED_PARENT) {
-                            return {...n, selected: NODE_ENUM.NODE_SELECTED};
-                        } else {
-                            return {...n}
-                        }
-                    }),
+            if (node.id === id) {
+                switch (selected) {
+                    case NODE_ENUM.NODE_HAS_SELECTED_CHILDREN: // fallthrough
+                    case NODE_ENUM.NODE_DESELECTED:
+                        return {
+                            ...setAll(node, NODE_ENUM.NODE_HAS_SELECTED_PARENT),
+                            selected: NODE_ENUM.NODE_SELECTED,
+                        };
+                    case NODE_ENUM.NODE_HAS_SELECTED_PARENT: // fallthrough
+                    case NODE_ENUM.NODE_SELECTED:
+                        return {
+                            ...setAll(node, NODE_ENUM.NODE_DESELECTED),
+                        };
+                    default:
+                        return {...node};
                 }
             }
+
+            const {children = []} = node;
+            const newChildren = children.map(n => toggleSelectionRecursive(n, nodeId));
+
+            if (newChildren.length > 0) {
+                if (newChildren.every(isSelected)) {
+                    if (node.data) {
+                        return {
+                            ...node,
+                            selected: NODE_ENUM.NODE_SELECTED,
+                            children: newChildren.map(n => ({...n, selected: NODE_ENUM.NODE_HAS_SELECTED_PARENT})),
+                        }
+                    } else {
+                        // This is a special case when you have a tree
+                        // that doesn't have a root element data,
+                        // only a list of children elements.
+                        return {
+                            ...node,
+                            selected: NODE_ENUM.NODE_SELECTED,
+                            children: newChildren,
+                        }
+                    }
+                } else if (newChildren.every(isDeselected)) {
+                    return {
+                        ...node,
+                        selected: NODE_ENUM.NODE_DESELECTED,
+                        children: newChildren,
+                    }
+                } else if (newChildren.some(n => (hasSelectedChildren(n) || isSelected(n)))) {
+                    return {
+                        ...node,
+                        selected: NODE_ENUM.NODE_HAS_SELECTED_CHILDREN,
+                        children: newChildren.map(n => {
+                            if (n.selected === NODE_ENUM.NODE_HAS_SELECTED_PARENT) {
+                                return {...n, selected: NODE_ENUM.NODE_SELECTED};
+                            } else {
+                                return {...n}
+                            }
+                        }),
+                    }
+                } else if (newChildren.some(hasSelectedParent) && newChildren.some(isDeselected)) {
+                    return {
+                        ...node,
+                        selected: NODE_ENUM.NODE_HAS_SELECTED_CHILDREN,
+                        children: newChildren.map(n => {
+                            if (n.selected === NODE_ENUM.NODE_HAS_SELECTED_PARENT) {
+                                return {...n, selected: NODE_ENUM.NODE_SELECTED};
+                            } else {
+                                return {...n}
+                            }
+                        }),
+                    }
+                }
+            }
+
+            return {...node, children: newChildren};
         }
 
-        return {...node, children: newChildren};
+        return {...toggleSelectionRecursive(node, nodeId)};
     }
 
-    return {...processOneNode(node, nodeId)};
+    return {...state, tree: toggleSelectionInner(state.tree, action.nodeId)};
 }
 
 
@@ -129,67 +154,71 @@ function comparator(x, y) {
     }
 }
 
+function filterTreeNodes(state, action) {
+    function filterTree(node, data) {
+        const {children = []} = node;
 
-function filterTree(node, data) {
-    const {children = []} = node;
-
-    const {contain, markStart, markEnd} = comparator(node.data, data);
-    if (contain) {
-        return {
-            ...node,
-            markStart,
-            markEnd,
+        const {contain, markStart, markEnd} = comparator(node.data, data);
+        if (contain) {
+            return {
+                ...node,
+                markStart,
+                markEnd,
+            }
+        } else if (children.length === 0) {
+            return null;
         }
-    } else if (children.length === 0) {
+
+        let newChildren = children.map(
+            n => filterTree(n, data)
+        ).filter(
+            n => (n !== null)
+        );
+        if (newChildren.length > 0) {
+            return {
+                ...node,
+                collapsed: false,
+                children: newChildren,
+            };
+        }
         return null;
     }
 
-    let newChildren = children.map(
-        n => filterTree(n, data)
-    ).filter(
-        n => (n !== null)
-    );
-    if (newChildren.length > 0) {
+    if (!action.data) {
         return {
-            ...node,
-            collapsed: false,
-            children: newChildren,
-        };
+            ...state,
+            tree: state.originalTreeCache,
+            originalTreeCache: null,
+        }
     }
-    return null;
+    let res = {...state};
+    if (!state.originalTreeCache) {
+        res.originalTreeCache = res.tree;
+    }
+    res.tree = filterTree(res.originalTreeCache, action.data);
+    return res;
 }
 
-// todo: add default state
-export const treeReducer = (state, action) => {
-    const {tree} = state;
 
+export const treeReducer = (
+    state = {
+        originalTreeCache: null,
+        tree: {
+            id: 0,
+            data: null,
+            children: [],
+        },
+        selectedItems: [],
+    },
+    action
+) => {
     switch (action.type) {
         case TOGGLE_COLLAPSE_EXPAND:
-            return {
-                ...state,
-                tree: toggleCollapseNode(tree, action.nodeId),
-            };
+            return toggleCollapseTreeNode(state, action);
         case TOGGLE_SELECT_DESELECT:
-            return {
-                ...state,
-                tree: toggleSelectNode(tree, action.nodeId),
-            };
+            return toggleSelectionOfTreeNode(state, action);
         case FILTER_TREE_NODES:
-            if (!action.data) {
-                return {
-                    ...state,
-                    tree: state.originalTreeCache,
-                    originalTreeCache: null,
-                }
-            }
-            let res = {
-                ...state,
-            };
-            if (!state.originalTreeCache) {
-                res.originalTreeCache = res.tree;
-            }
-            res.tree = filterTree(res.originalTreeCache, action.data);
-            return res;
+            return filterTreeNodes(state, action);
         default:
             return state;
     }
