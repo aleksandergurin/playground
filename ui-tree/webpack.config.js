19e417fc180cbeac4514fc4238c15aa14dacfb5e
a.gurin <a.gurin@smartweb.com.ua>
2016-11-24 21:39:29 +0200

    ui-tree changes

--- ui-tree/webpack.config.js
+++ ui-tree/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.jsx?$/,
+                exclude: '/node_modules',
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015', 'stage-2', 'react']
